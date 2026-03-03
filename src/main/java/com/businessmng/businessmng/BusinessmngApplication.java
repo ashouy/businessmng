923833dce92ef520e3d281b1fc085c5eb6413395
joaopaulo <joaopaulokad@gmail.com>
2026-03-03 20:10:51 -0300

    Initialize Spring Boot project with Maven wrapper and basic structure
    - Added .gitattributes and .gitignore files for project configuration.
    - Introduced Maven wrapper scripts (mvnw and mvnw.cmd) for easier Maven management.
    - Created pom.xml with Spring Boot dependencies and configuration.
    - Added main application class (BusinessmngApplication) and servlet initializer (ServletInitializer).
    - Included application properties and a test class for context loading verification.
    - Set up Maven wrapper properties for versioning and distribution URL.

---
+++ src/main/java/com/businessmng/businessmng/BusinessmngApplication.java
@@ -0,0 +1,13 @@
+package com.businessmng.businessmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessmngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessmngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/businessmng/businessmng/BusinessmngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businessmng.businessmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessmngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
